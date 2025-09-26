e8c35f1fe6c36cc41aa6c7d5ce122c22bb3d3c66
Jaxic <kerphlam@gmail.com>
2025-09-26 10:11:53 -0600

    Sitemap created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://womenscoldplunge.com',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 45000,
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
