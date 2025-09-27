7b8249eb3e8ad9f65326c2c991d65e4fa99f6da9
Jaxic <kerphlam@gmail.com>
2025-09-26 22:17:51 -0600

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://womenscoldplunge.com',
+  site: 'https://www.womenscoldwellness.com',
   integrations: [
     sitemap({
       changefreq: 'weekly',
